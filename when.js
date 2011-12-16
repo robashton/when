3945928b334f26f4fa7d20dcd394c4b68d67266d
Rob Ashton <robashton@codeofrob.com>
2011-12-16 16:12:49 +0100

    Annotated examples and colours

--- when.js
+++ when.js
@@ -1,5 +1,8 @@
+var ConsoleOutput = require('./lib/consoleoutput.js').ConsoleOutput;
+
 var cleanMessageFromCallback = function(statement) {
-  var startIndex = statement.indexOf('this.') + 5;
+  var startIndex = statement.indexOf('this.');
+  startIndex = startIndex < 0 ? 0 : startIndex + 5;
   var endIndex = statement.indexOf(')', startIndex) + 1;
   return statement.substr(startIndex, endIndex - startIndex);
 };
@@ -17,7 +20,7 @@ var when = function(testName, callback) {
   var self = this;
   var statements = [];
   var expectedMessages = parseCallbackForExpectedMessages(callback);
- 
+  
   var addStatement = function(statement, valid) {
     statements.push({ valid: valid, text: statement});
     tryAndFinish();
@@ -25,18 +28,15 @@ var when = function(testName, callback) {
 
   var tryAndFinish = function() {
     if(statements.length < expectedMessages.length) return;
-    console.log('When ' + testName);
+    when.output.writeWhen(testName);
     for(var i = 0; i < statements.length; i++) {
       printStatement(statements[i]);
     }
+    when.output.endWhen();
   };
 
   var printStatement = function(statement) {
-    if(statement.valid) {
-      console.log('\t then ' + statement.text);
-    } else {
-      console.warn('\t then ' + statement.text);
-    }
+    when.output.writeThen(statement.text, statement.valid);
   };
 
   var lookupStatementForCurrentFunction = function() {
@@ -50,6 +50,7 @@ var when = function(testName, callback) {
     });
   };
 
+  when.output.beginWhen();
   callback(function(statement, validity) {
     if(statement instanceof Function)
       processStatementAsFunction(statement, validity);
@@ -57,5 +58,25 @@ var when = function(testName, callback) {
      addStatement(statement, validity);
   });
 };
+when.output = new ConsoleOutput();
+
+when.allTestsFinished = function(callback) {
+  when.output.whenAllGivensMarkedComplete(callback);
+};
+
+when.printReport = function() {
+  var failedTestCount = when.output.passedTests;
+  var passedTestCount = when.output.failedTests; 
+  var total = failedTestCount + passedTestCount;
+
+  if(failedTestCount > 0) {
+    var text = '\nTest run failed: (' + passedTestCount + '/' + total + ')';
+    console.log(text.red);
+  } else {
+    var text = '\nTest run passed: (' + passedTestCount + '/' + total + ')';
+    console.log(text.green);
+  }
+};
+
 
 exports.when = when;
